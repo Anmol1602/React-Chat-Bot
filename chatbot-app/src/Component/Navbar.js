c13fe4fb9afa5c4861b09ae51c3538f944d85b93
Anmol1607 <anmol.varshney@gupshup.io>
2024-12-22 22:08:46 +0530

    Added icons in Navbar

--- chatbot-app/src/Component/Navbar.js
+++ chatbot-app/src/Component/Navbar.js
@@ -1,11 +1,18 @@
-import React , { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 import logo from "../assets/Logo.webp"; // Replace with your logo file path
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import ContactMailIcon from "@mui/icons-material/ContactMail";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import LoginIcon from "@mui/icons-material/Login";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+
 
 function Navbar({ user, setUser }) {
-     
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
         if (storedUser) {
@@ -27,24 +34,42 @@ function Navbar({ user, setUser }) {
             <div className="navbar-links">
                 <ul className="navbar-list">
                     <li className="navbar-item">
-                        <Link to="/" className="navbar-link">Home</Link>
+                        <Link to="/" className="navbar-link">
+                            <HomeIcon style={{ fontSize: "30px" }} />
+                            <span>Home</span>
+                        </Link>
                     </li>
                     <li className="navbar-item">
-                        <Link to="/about" className="navbar-link">About</Link>
+                        <Link to="/about" className="navbar-link">
+                            <InfoIcon style={{ fontSize: "30px" }} />
+                            <span>About</span>
+                        </Link>
                     </li>
                     <li className="navbar-item">
-                        <Link to="/contact" className="navbar-link">Contact</Link>
+                        <Link to="/contact" className="navbar-link">
+                            <ContactMailIcon style={{ fontSize: "30px" }} />
+                            <span>Contact</span>
+                        </Link>
                     </li>
                     <li className="navbar-item">
-                        <Link to="/pricing" className="navbar-link">Pricing</Link>
+                        <Link to="/pricing" className="navbar-link">
+                            <AttachMoneyIcon style={{ fontSize: "30px" }} />
+                            <span>Pricing</span>
+                        </Link>
                     </li>
                     {!user ? (
                         <>
                             <li className="navbar-item">
-                                <Link to="/signup" className="navbar-link">Sign Up</Link>
+                                <Link to="/signup" className="navbar-link">
+                                    <PersonAddIcon style={{ fontSize: "30px" }} />
+                                    <span>Sign Up</span>
+                                </Link>
                             </li>
                             <li className="navbar-item">
-                                <Link to="/login" className="navbar-link">Login</Link>
+                                <Link to="/login" className="navbar-link">
+                                    <LoginIcon style={{ fontSize: "30px" }} />
+                                    <span>Login</span>
+                                </Link>
                             </li>
                         </>
                     ) : (
@@ -52,11 +77,13 @@ function Navbar({ user, setUser }) {
                             <li className="navbar-item">
                                 <Link to="/profile" className="navbar-link">
                                     <AccountCircleIcon style={{ fontSize: "30px" }} />
+                                    <span>Profile</span>
                                 </Link>
                             </li>
                             <li className="navbar-item">
                                 <button className="navbar-link" onClick={handleLogout}>
-                                    Logout
+                                        <ExitToAppIcon style={{ fontSize: "30px" }} />
+                                        <span>Logout</span>
                                 </button>
                             </li>
                         </>
